c3a749dfe4d4186f99581e11ec365d5f97ae9ddd
Dawud Rehan <d.rehan7@gmail.com>
2021-02-21 10:16:20 -0500

    all paths drawn with 'optimal' one different color. needs clean up

--- pathfinder.py
+++ pathfinder.py
@@ -6,27 +6,29 @@ LARGE_MOUNTAIN = 'elevation_large.txt'
 
 
 # Method and opens file and returns data inside
+
+
 def open_file(file):
     with open(file, 'r') as f:
         entire_text = f.readlines()
-        return entire_text
+        elevations_array = [x.split() for x in entire_text]
+
+        # print(elevations_array)
+        return elevations_array
 
 
 # method to get dimension of final picture based off length of input
 # small txt return 600,600 | large txt return 1201,1201
-def get_dimensions(file_contents):
-    entire_text = file_contents
-    return len(entire_text), len(entire_text[0].split())
+def get_dimensions(elevation_array):
+    return len(elevation_array), len(elevation_array[0])
 
 
-def get_min_elevation(file):
-    with open(file, 'r') as test_file:
-        return min(test_file.read().split())
-
-
-def get_max_elevation(file):
-    with open(file, 'r') as test_file:
-        return max(test_file.read().split())
+def get_min_and_max_elevation(elevation_array):
+    temp = []
+    for x in elevation_array:
+        for y in x:
+            temp.append(int(y))
+    return (min(temp), max(temp))
 
 
 # returns tuple of 3 integers representing the same color value i.e. (100,100,100)
@@ -37,35 +39,31 @@ def get_color_value(elevation, minimum, maximum):
     return (int(result), int(result), int(result))
 
 
-def create_picture(file):
-    file_contents = open_file(file)
-    dimensions = get_dimensions(file_contents)
+def create_picture(elevation_array):
+    dimensions = get_dimensions(elevation_array)
     im = Image.new("RGBA", (dimensions))
-    minimum = get_min_elevation(file)
-    maximum = get_max_elevation(file)
+    minimum = get_min_and_max_elevation(elevation_array)[0]
+    maximum = get_min_and_max_elevation(elevation_array)[1]
 
     for x in range(dimensions[0]):
-        for y in range(dimensions[1]):
-            im.putpixel((x, y), get_color_value(
-                file_contents[x].split()[y], minimum, maximum))
+        for y in range(dimensions[0]):
+            # ? no idea why y,x
+            im.putpixel((y, x), get_color_value(
+                elevation_array[x][y], minimum, maximum))
 
-    im.save(file[0:len(file)-4] + '.png')
+    im.save(str(dimensions[0]) + "_mountain.png")
+
+
+def value_at_coordinates(elevation_array, current_coord):
+    return int(elevation_array[current_coord[0]][current_coord[1]])
+
+
+# create_picture(open_file(SMALL_MOUNTAIN))
+# create_picture(open_file(LARGE_MOUNTAIN))
 
 
 # method for testing logic
-def f_around_method(color_value):
-    im = Image.new("RGBA", (100, 100))
-
-    print(im.getpixel((0, 0)))
-
-    for x in range(100):
-        for y in range(50):
-            im.putpixel((x, y), (color_value, color_value, color_value))
-
-    for x in range(100):
-        for y in range(50, 100):
-            im.putpixel((x, y), (100, 100, 100))
 
 
-create_picture(SMALL_MOUNTAIN)
-create_picture(LARGE_MOUNTAIN)
+def f_around_method():
+    pass
